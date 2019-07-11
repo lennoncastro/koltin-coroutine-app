92965f26f174cae201533abcd86cca12597b03e0
lennon <lennon.castro.92@gmail.com>
2019-07-11 19:45:27 -0300

    adiciona viewmodel provider em module

--- app/src/main/java/com/lennon/kotlincoroutine/di/module.kt
+++ app/src/main/java/com/lennon/kotlincoroutine/di/module.kt
@@ -2,10 +2,11 @@ package com.lennon.kotlincoroutine.di
 
 import com.lennon.kotlincoroutine.RetrofitClient
 import com.lennon.kotlincoroutine.viewmodel.RepositoryViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val myModule : Module = module {
-    factory { RepositoryViewModel() }
+    viewModel { RepositoryViewModel() }
     single { RetrofitClient }
 }
