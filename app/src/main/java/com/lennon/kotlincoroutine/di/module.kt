d448a85ef7c9b01efca4f5803b0575ee1ec76358
lennon <lennon.castro.92@gmail.com>
2019-07-11 15:25:16 -0300

    adiciona repository ao módulo

--- app/src/main/java/com/lennon/kotlincoroutine/di/module.kt
+++ app/src/main/java/com/lennon/kotlincoroutine/di/module.kt
@@ -1,9 +1,11 @@
 package com.lennon.kotlincoroutine.di
 
+import com.lennon.kotlincoroutine.RetrofitClient
 import com.lennon.kotlincoroutine.viewmodel.RepositoryViewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val myModule : Module = module {
-    single { RepositoryViewModel() }
+    factory { RepositoryViewModel() }
+    single { RetrofitClient }
 }
