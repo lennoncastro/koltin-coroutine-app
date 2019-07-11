6a05f7b50317f110061dcc4da9f277ad956e07c0
lennon <lennon.castro.92@gmail.com>
2019-07-11 19:46:51 -0300

    renome mainactivity para homeactivity

---
+++ app/src/main/java/com/lennon/kotlincoroutine/ui/HomeActivity.kt
@@ -0,0 +1,33 @@
+package com.lennon.kotlincoroutine.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.lennon.kotlincoroutine.R
+import com.lennon.kotlincoroutine.viewmodel.RepositoryViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+
+class HomeActivity : AppCompatActivity() {
+
+    private val repositoryViewModel: RepositoryViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        repositoryViewModel.fetchRepositories()
+        onSuccessObserver()
+    }
+
+    private fun onSuccessObserver() {
+        repositoryViewModel.response.observe(this, Observer {
+            it?.let {
+                repositories.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+                repositories.adapter = RepositoryAdapter(it)
+            }
+        })
+    }
+}
--- app/src/main/java/com/lennon/kotlincoroutine/viewmodel/RepositoryViewModel.kt
+++ app/src/main/java/com/lennon/kotlincoroutine/viewmodel/RepositoryViewModel.kt
@@ -1,5 +1,31 @@
 package com.lennon.kotlincoroutine.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.lennon.kotlincoroutine.data.RepositoryImpl
+import com.lennon.kotlincoroutine.data.model.vo.RepositoryVO
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class RepositoryViewModel: ViewModel()
+class RepositoryViewModel : ViewModel(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext = Main
+
+    private val viewModelJob = SupervisorJob()
+
+    private val viewModeScope = CoroutineScope(Main + viewModelJob)
+
+    var response = MutableLiveData<List<RepositoryVO>>()
+
+    var repository = RepositoryImpl()
+
+    fun fetchRepositories() {
+        viewModeScope.launch {
+            response.value = repository.fetchRepositories("Java", 0)
+        }
+    }
+
+}
