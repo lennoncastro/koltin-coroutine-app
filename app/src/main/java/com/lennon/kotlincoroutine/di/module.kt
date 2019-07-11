6bb4758bfa42313fe40e9f4597115111f10b087e
lennon <lennon.castro.92@gmail.com>
2019-07-11 19:58:35 -0300

    refatora viewmodel

--- app/src/main/java/com/lennon/kotlincoroutine/di/module.kt
+++ app/src/main/java/com/lennon/kotlincoroutine/di/module.kt
@@ -1,6 +1,7 @@
 package com.lennon.kotlincoroutine.di
 
 import com.lennon.kotlincoroutine.RetrofitClient
+import com.lennon.kotlincoroutine.data.RepositoryImpl
 import com.lennon.kotlincoroutine.viewmodel.RepositoryViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
@@ -9,4 +10,5 @@ import org.koin.dsl.module
 val myModule : Module = module {
     viewModel { RepositoryViewModel() }
     single { RetrofitClient }
+    factory { RepositoryImpl() }
 }
