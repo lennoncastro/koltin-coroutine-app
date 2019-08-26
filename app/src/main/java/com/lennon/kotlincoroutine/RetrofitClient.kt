1364cc521fd22d73ac1dcd0b7e8eb4a7283da327
lennon castro <lennon.castro.92@gmail.com>
2019-08-26 16:12:29 -0300

    atualiza retrofit client

--- app/src/main/java/com/lennon/kotlincoroutine/RetrofitClient.kt
+++ app/src/main/java/com/lennon/kotlincoroutine/RetrofitClient.kt
@@ -6,8 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
+    private const val BASE_URL = "https://api.github.com"
+
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://api.github.com")
+        .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .build()
